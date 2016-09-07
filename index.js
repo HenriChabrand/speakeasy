38256d07a181a33e3546bc7cab366a86e11a1d0c
HenriChabrand <HenriChabrand@users.noreply.github.com>
2016-09-07 18:23:08 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+var speak = require("./speakeasy-nlp");
+console.log('Yo esay');
+
+// Analyze sentences at a basic level
+// ------------------------------------- //
+console.log(speak.classify("What is your name?"));    
